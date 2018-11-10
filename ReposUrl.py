5528e0212704cc12696fe0cbc88140fbf451bdc5
Suraj <suraj.aavula1993@gmail.com>
2018-11-10 10:25:21 +0530

    Update ReposUrl.py

--- ReposUrl.py
+++ ReposUrl.py
@@ -7,15 +7,8 @@ df=pd.read_csv("file.csv",encoding="cp1252")
 repos_url=df.iloc[1:,3]
 for url in repos_url:
     #print(url)
-#token1='9d98deacd0b4b3b3efdfb254c6af5c931ec237c5'
-#token2='808fd391c6908d19ceca268a311f3cb53a92b4e0'
-#Naveen's token: '19d3613f87d6be4b31d6480ac9aa6692aa47cd47'
-#Naveen's new token: '8103f0da6d0cd4dda76ba3960fd635ad82a74e34'
-#Vivek's Token: '0d494f03e8aeab180598b9f6cfd6c53ee56b9190'
-#Suraj New Token: 'bcda4a9669f4c0bba564f7d8d90bdbe6be90ef3d'
-#Noopur's Token:'3e24f3ad2a447fddd55bc549718c74f030599222'
-#Ramy's Token: 'c45709566c371d0d77a4a121857d3813fec5ae45'
-    headers = {'Authorization': '19d3613f87d6be4b31d6480ac9aa6692aa47cd47 %s'}
+
+    headers = {'Authorization': 'Authentication_token %s'}
     response2 = requests.get(url, headers=headers)
     jsonToDict = response2.json()	#coverting json response to a python string
     repos_data=[]
